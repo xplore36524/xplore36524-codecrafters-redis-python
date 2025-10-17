0b1a95239c6204630f831079ad7afe9badb273d2
Ishan Ray <ishanray1234@gmail.com>
2025-10-17 19:56:50 +0530

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -7,9 +7,19 @@ def main():
 
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
     connection, _ = server_socket.accept()
+    print(connection)
 
     # Send a response to the client
-    connection.sendall(b"+PONG\r\n")
+    while True:
+        data = connection.recv(1024)
+        if not data:
+            break
+        print(f"Received data: {data}")
+        # if data.startswith(b"PING"):
+        connection.sendall(b"+PONG\r\n")
+        # elif data.startswith(b"QUIT"):
+        #     connection.sendall(b"+OK\r\n")
+        #     break
 
 
 if __name__ == "__main__":
