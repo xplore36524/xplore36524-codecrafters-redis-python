24d2143cead076aafa3cd48bd920a4a80dbb284b
Ishan Ray <ishanray1234@gmail.com>
2025-10-19 12:28:32 +0530

    codecrafters submit [skip ci]

--- app/utils2.py
+++ app/utils2.py
@@ -18,15 +18,44 @@ def checker(key, id_time, id_sequence):
             return "ERR The ID specified in XADD is equal or smaller than the target stream top item"
     return "ok"
 
+def allot(key, id_time):
+    """Allots a new ID based on the provided id_time."""
+    last_entry = streams.get(key, [])
+    if last_entry:
+        last_id, last_sequence = last_entry[-1].get("id").split("-")
+        if int(id_time) < int(last_id):
+            new_time = int(last_id)
+            new_sequence = int(last_sequence) + 1
+        elif int(id_time) == int(last_id):
+            new_time = int(last_id)
+            new_sequence = int(last_sequence) + 1
+        else:
+            new_time = int(id_time)
+            new_sequence = int(0)
+    else:
+        new_time = int(id_time)
+        if new_time > 0:
+            new_sequence = int(0)
+        else:
+            new_sequence = int(1)
+    return f"{new_time}-{new_sequence}"
 
 def xadd(info):
     """Adds an entry to a stream."""
     key = info[0]
     id = info[1]
-    id_time, id_sequence = id.split("-")
-    check = checker(key, id_time, id_sequence)
-    if check != "ok":
-        return "err", check
+    if id != '*':
+        id_time, id_sequence = id.split("-")
+        if id_sequence == '*':
+            id_time, id_sequence = id.split("-")
+            time_allot = allot(key, id_time)
+            id = time_allot
+        else:
+            check = checker(key, id_time, id_sequence)
+            if check != "ok":
+                return "err", check
+    else:
+        pass
     field_values = info[2:]
 
     if key not in streams:
@@ -39,7 +68,7 @@ def xadd(info):
         entry[field] = value
 
     streams[key].append(entry)
-    return "id", id  # In a real Redis implementation, this would be the entry ID
+    return "id", id  # In a real Redis implementation, this would be the entry ID  
 
 def type_getter_streams(key):
     """Returns the type of the value stored at key."""
