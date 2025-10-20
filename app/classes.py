333a0f625d92e608ff44094c4a23708d84dff978
Ishan Ray <ishanray1234@gmail.com>
2025-10-20 22:25:20 +0530

    codecrafters submit [skip ci]

--- app/classes.py
+++ app/classes.py
@@ -34,7 +34,7 @@ class Slave():
 
         server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        server_socket.bind(("localhost", int(self.args.port, self.config)))
+        server_socket.bind(("localhost", int(self.args.port)))
         server_socket.listen(10)
 
         # connect to master
