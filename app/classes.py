be7e0698f49f6d56a4169284eb181f4a22d7ac2f
Ishan Ray <ishanray1234@gmail.com>
2025-10-20 22:16:00 +0530

    codecrafters submit [skip ci]

--- app/classes.py
+++ app/classes.py
@@ -1,6 +1,7 @@
 import socket
 import threading
 from app.handler import handle_client
+from app.resp import resp_parser
 
 class Master():
     def __init__(self, args):
@@ -27,11 +28,20 @@ class Slave():
         self.args = args
         self.config = {}
         self.config['role'] = 'slave'
+        master_host, master_port = self.args.replicaof.split(' ')
+        self.config['master_host'] = master_host
+        self.config['master_port'] = master_port
 
         server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        server_socket.bind(("localhost", int(self.args.port)))
+        server_socket.bind(("localhost", int(self.args.port, self.config)))
         server_socket.listen(10)
+
+        # connect to master
+        master_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        master_socket.connect((self.config['master_host'], int(self.config['master_port'])))
+        master_socket.send(resp_parser(['PING']))
+
 
         while True:
             client_socket, _ = server_socket.accept()
