289defb87ba1beaafde25f03ae355892a0690bca
Ishan Ray <ishanray1234@gmail.com>
2025-10-21 00:00:35 +0530

    codecrafters submit [skip ci]

--- app/handler.py
+++ app/handler.py
@@ -6,6 +6,8 @@ blocked = {}
 blocked_xread = {}
 queue = []
 
+RDB_hex = '524544495330303131fa0972656469732d76657205372e322e30fa0a72656469732d62697473c040fa056374696d65c26d08bc65fa08757365642d6d656dc2b0c41000fa08616f662d62617365c000fff06e3bfec0ff5aa2'
+
 def cmd_executor(decoded_data, connection, config, queued, executing):
     print(f"decoded_data: {decoded_data}")
     # EXEC Checker
@@ -252,6 +254,14 @@ def cmd_executor(decoded_data, connection, config, queued, executing):
         response = simple_string_encoder(response)
         # if executing:
         #     return response, queued
+        connection.sendall(response)
+        response = b"$"
+        # decode RDB_hex to binary
+        length = str(len(bytes.fromhex(RDB_hex))).encode('utf-8')
+        response += length
+        response += b"\r\n"
+        response += bytes.fromhex(RDB_hex)
+        print(f"PSYNC response: {response}")
         connection.sendall(response)
         return [], queued
 
