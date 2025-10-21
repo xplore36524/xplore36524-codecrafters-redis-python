3c2a02bfac715c028243356cee76f03a744589d6
Ishan Ray <ishanray1234@gmail.com>
2025-10-21 19:29:22 +0530

    codecrafters submit [skip ci]

--- app/handler.py
+++ app/handler.py
@@ -11,6 +11,7 @@ BYTES_READ = 0
 RDB_hex = '524544495330303131fa0972656469732d76657205372e322e30fa0a72656469732d62697473c040fa056374696d65c26d08bc65fa08757365642d6d656dc2b0c41000fa08616f662d62617365c000fff06e3bfec0ff5aa2'
 
 def cmd_executor(decoded_data, connection, config, queued, executing):
+    global BYTES_READ
     print(f"decoded_data: {decoded_data}")
     # EXEC Checker
     if queued and decoded_data[0] != "EXEC" and decoded_data[0] != "DISCARD":
