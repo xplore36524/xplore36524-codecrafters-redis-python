eb1c9a63daa13fe12112c33148bca496199987d3
Ishan Ray <ishanray1234@gmail.com>
2025-10-22 21:34:56 +0530

    codecrafters submit [skip ci]

--- app/handler.py
+++ app/handler.py
@@ -327,6 +327,7 @@ def cmd_executor(decoded_data, connection, config, queued, executing):
     # CONGIG GET
     elif decoded_data[0].upper() == "CONFIG":
         print(f"CONFIG command")
+        response = ""
         if decoded_data[2].upper() == "dir":
             response = resp_encoder(["dir",config['dir']])
         elif decoded_data[2].upper() == "dbfilename":
