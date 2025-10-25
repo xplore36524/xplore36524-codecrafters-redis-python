d252feedb600854952f3a61ead5d691370250762
Ishan Ray <ishanray1234@gmail.com>
2025-10-25 20:22:55 +0530

    codecrafters submit [skip ci]

--- app/handler.py
+++ app/handler.py
@@ -38,7 +38,7 @@ def cmd_executor(decoded_data, connection, config, queued, executing):
         elif decoded_data[0].upper() == "QUIT":
             pass
         else:
-            response = (f"-ERR Can't execute {decoded_data[0]}").encode()
+            response = (f"-ERR Can't execute '{decoded_data[0].tolower()}'").encode()
             connection.sendall(response)
         return [], queued
     if queued and decoded_data[0] != "EXEC" and decoded_data[0] != "DISCARD":
